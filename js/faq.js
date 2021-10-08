beb47f64ab82351119aec4fb99207b5aca28e572
Aditya Sarawat <54805791+aditya-sarawat@users.noreply.github.com>
2021-10-08 22:17:33 +0530

    Version 0.1

---
+++ js/faq.js
@@ -0,0 +1,164 @@
+let btn0 = document.getElementsByClassName("btn0")
+let btn1 = document.getElementsByClassName("btn1");
+let btn2 = document.getElementsByClassName("btn2");
+let btn3 = document.getElementsByClassName("btn3");
+let btn4 = document.getElementsByClassName("btn4");
+let btn5 = document.getElementsByClassName("btn5");
+let btn6 = document.getElementsByClassName("btn6");
+let btn7 = document.getElementsByClassName("btn7");
+let btn8 = document.getElementsByClassName("btn8");
+let btn9 = document.getElementsByClassName("btn9");
+let btn10 = document.getElementsByClassName("btn10");
+let btn11 = document.getElementsByClassName("btn11");
+let btn12 = document.getElementsByClassName("btn12");
+let btn13 = document.getElementsByClassName("btn13");
+let btn14 = document.getElementsByClassName("btn14");
+let btn15 = document.getElementsByClassName("btn15");
+
+let show0 = document.getElementById("collapseZero");
+let show1 = document.getElementById("collapseOne");
+let show2 = document.getElementById("collapseTwo");
+let show3 = document.getElementById("collapseThree");
+let show4 = document.getElementById("collapseFour");
+let show5 = document.getElementById("collapseFive");
+let show6 = document.getElementById("collapseSix");
+let show7 = document.getElementById("collapseSeven");
+let show8 = document.getElementById("collapseEight");
+let show9 = document.getElementById("collapseNine");
+let show10 = document.getElementById("collapseTen");
+let show11 = document.getElementById("collapseEleven");
+let show12 = document.getElementById("collapseTwelve");
+let show13 = document.getElementById("collapseThirteen");
+let show14 = document.getElementById("collapseFourteen");
+let show15 = document.getElementById("collapseFifteen");
+
+// console.log(show1);
+// console.log(show2);
+// console.log(show3);
+// console.log(show4);
+// console.log(show5);
+// console.log(show6);
+// console.log(show7);
+// console.log(show8);
+// console.log(show9);
+// console.log(show10);
+// console.log(show11);
+// console.log(show12);
+// console.log(show13);
+// console.log(show14);
+// console.log(show15);
+
+btn0[0].onclick = function (event) {
+  if (show0.classList.contains("show")) {
+    btn0[0].classList.remove("btn-link-clicked");
+  } else {
+    btn0[0].classList.add("btn-link-clicked");
+  }
+};
+
+btn1[0].onclick = function (event) {
+  if (show1.classList.contains("show")) {
+    btn1[0].classList.remove("btn-link-clicked");
+  } else {
+    btn1[0].classList.add("btn-link-clicked");
+  }
+};
+
+btn2[0].onclick = function (event) {
+  if (show2.classList.contains("show")) {
+    btn2[0].classList.remove("btn-link-clicked");
+  } else {
+    btn2[0].classList.add("btn-link-clicked");
+  }
+};
+btn3[0].onclick = function (event) {
+  if (show3.classList.contains("show")) {
+    btn3[0].classList.remove("btn-link-clicked");
+  } else {
+    btn3[0].classList.add("btn-link-clicked");
+  }
+};
+btn4[0].onclick = function (event) {
+  if (show4.classList.contains("show")) {
+    btn4[0].classList.remove("btn-link-clicked");
+  } else {
+    btn4[0].classList.add("btn-link-clicked");
+  }
+};
+btn5[0].onclick = function (event) {
+  if (show5.classList.contains("show")) {
+    btn5[0].classList.remove("btn-link-clicked");
+  } else {
+    btn5[0].classList.add("btn-link-clicked");
+  }
+};
+btn6[0].onclick = function (event) {
+  if (show6.classList.contains("show")) {
+    btn6[0].classList.remove("btn-link-clicked");
+  } else {
+    btn6[0].classList.add("btn-link-clicked");
+  }
+};
+btn7[0].onclick = function (event) {
+  if (show7.classList.contains("show")) {
+    btn7[0].classList.remove("btn-link-clicked");
+  } else {
+    btn7[0].classList.add("btn-link-clicked");
+  }
+};
+btn8[0].onclick = function (event) {
+  if (show8.classList.contains("show")) {
+    btn8[0].classList.remove("btn-link-clicked");
+  } else {
+    btn8[0].classList.add("btn-link-clicked");
+  }
+};
+btn9[0].onclick = function (event) {
+  if (show9.classList.contains("show")) {
+    btn9[0].classList.remove("btn-link-clicked");
+  } else {
+    btn9[0].classList.add("btn-link-clicked");
+  }
+};
+btn10[0].onclick = function (event) {
+  if (show10.classList.contains("show")) {
+    btn10[0].classList.remove("btn-link-clicked");
+  } else {
+    btn10[0].classList.add("btn-link-clicked");
+  }
+};
+btn11[0].onclick = function (event) {
+  if (show11.classList.contains("show")) {
+    btn11[0].classList.remove("btn-link-clicked");
+  } else {
+    btn11[0].classList.add("btn-link-clicked");
+  }
+};
+btn12[0].onclick = function (event) {
+  if (show12.classList.contains("show")) {
+    btn12[0].classList.remove("btn-link-clicked");
+  } else {
+    btn12[0].classList.add("btn-link-clicked");
+  }
+};
+btn13[0].onclick = function (event) {
+  if (show13.classList.contains("show")) {
+    btn13[0].classList.remove("btn-link-clicked");
+  } else {
+    btn13[0].classList.add("btn-link-clicked");
+  }
+};
+btn14[0].onclick = function (event) {
+  if (show14.classList.contains("show")) {
+    btn14[0].classList.remove("btn-link-clicked");
+  } else {
+    btn14[0].classList.add("btn-link-clicked");
+  }
+};
+btn15[0].onclick = function (event) {
+  if (show15.classList.contains("show")) {
+    btn15[0].classList.remove("btn-link-clicked");
+  } else {
+    btn15[0].classList.add("btn-link-clicked");
+  }
+};
